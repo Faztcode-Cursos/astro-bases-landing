c9cc5844ddc101ba36b2517edfbeadcdf7da2e9f
JKmoises <moi.prado@duocuc.cl>
2023-12-10 23:22:09 -0300

    Deploy de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://Faztcode-Cursos.github.io',
+  base: '/astro-bases-landing',
 });
